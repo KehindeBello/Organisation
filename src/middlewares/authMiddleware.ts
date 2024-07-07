6e757bb9ce2c5286660563d43ccfc43d663a386a
kehindebello <adedotunomomeji@gmail.com>
2024-07-07 09:44:15 +0100

    add: route protection middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt";
+import { logger } from "../utils/logger";
+import { UserJwtPayload } from "../../interfaces/customRequest";
+
+
+export const protectRoute = (req: Request, res: Response, next: NextFunction) => {
+
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        logger.error(`Bearer token nissing!`);
+        return res.status(401).json({
+            "message": "Unauthorized, Bearer token missing!"
+        })
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded: UserJwtPayload = verifyToken(token);
+        req.user = decoded
+        logger.info(`log req user - ${decoded}`)
+        next();
+    } catch (error: any) {
+        logger.error(`Token verification failed: ${error.message}`);
+        return res.status(401).json({
+            "message": "Unauthorized, invalid token!"
+        })
+    }
+}
