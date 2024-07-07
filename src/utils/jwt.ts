1fcc66314d12379cd99bfa956d6e67928c21f973
kehindebello <adedotunomomeji@gmail.com>
2024-07-07 10:00:13 +0100

    modify: payload type

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,22 +1,18 @@
-import jwt, { SignOptions, Secret } from "jsonwebtoken";
+import jwt, { SignOptions} from "jsonwebtoken";
+import { UserJwtPayload } from "../../interfaces/customRequest";
 
 const secretKey = process.env.JWT_SECRET || 'secret'
 
-interface JwtPayload {
-    userId: string;
-    email: string;
-}
-
 //function to generate a JWT token
-export const generateToken = (payload: JwtPayload, expiresIn: string = '1h') => {
+export const generateToken = (payload: UserJwtPayload, expiresIn: string = '1h') => {
     const optons: SignOptions = {expiresIn};
     return jwt.sign(payload, secretKey, optons)
 }
 
 // function to verify tokem
-export const verifyToken = (token: string): JwtPayload => {
+export const verifyToken = (token: string): UserJwtPayload => {
     try {
-        return jwt.verify(token, secretKey) as JwtPayload
+        return jwt.verify(token, secretKey) as UserJwtPayload
     } catch (error) {
         throw new Error('Invalid Token');
     }
