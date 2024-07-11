72062f95dc3a19f19187cf23f46ad2d7385d3085
kehindebello <adedotunomomeji@gmail.com>
2024-07-11 15:21:07 +0100

    index file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import express, {Request, Response} from 'express';
-import fs from 'fs'
-import morgan from 'morgan'
-import path from 'path';
 import { UserRouter } from './routes/userRoute';
 import { AuthRouter } from './routes/authRoute';
 import { OrganisationRouter } from './routes/organisationRoute';
 
-const app = express();
+export const app = express();
 const port = process.env.PORT || 3000;
 
 //Parser middleware
 app.use(express.json())
 
-//logs
-const writeStream = fs.createWriteStream(path.join('./', "access.log"), {flags:"a"})
-app.use(morgan(":date - :method - :url - :status - :response-time ms", {stream: writeStream}))
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello, TypeScript!');
