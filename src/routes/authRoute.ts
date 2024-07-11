4a4aa5c868f54a731f3b3d2d89952e10d02c665f
kehindebello <adedotunomomeji@gmail.com>
2024-07-11 12:31:35 +0100

    added schema validation to route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/authController";
+import { validateSchema } from "../middlewares/validate";
+import { registerSchema } from "../../schemas/registerSchema";
+import { loginSchema } from "../../schemas/loginSchema";
 
 const router = Router();
 const authController = new AuthController
 
-router.post('/register', authController.post_signup);
-router.post('/login', authController.login_user);
+router.post('/register', validateSchema(registerSchema), authController.post_signup);
+router.post('/login', validateSchema(loginSchema), authController.login_user);
 
 export const AuthRouter = router;
