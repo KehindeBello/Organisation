d9727b831653e6daec91b016693ccde7965eade2
kehindebello <adedotunomomeji@gmail.com>
2024-07-11 12:32:24 +0100

    add user to org & schema validation

--- src/routes/organisationRoute.ts
+++ src/routes/organisationRoute.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { OrganisationController } from "../controllers/organisationController";
 import { protectRoute } from "../middlewares/authMiddleware";
+import { validateSchema } from "../middlewares/validate";
+import { organisationSchema } from "../../schemas/organisationSchema";
 
 const router = Router()
 const organisationController = new OrganisationController()
 
-router.post('/organisations', protectRoute, organisationController.create_organisation)
+router.post('/organisations', protectRoute, validateSchema(organisationSchema), organisationController.create_organisation)
 router.get('/organisations/:orgId?', protectRoute, organisationController.get_Organisation)
+router.post('/organisations/:orgId/users', organisationController.addUser)
 
 export const OrganisationRouter = router;
