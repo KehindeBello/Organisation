d418ba86163ed9ff61ad0e7a3342fdd4f4302758
kehindebello <adedotunomomeji@gmail.com>
2024-07-11 12:24:01 +0100

    add: add User to orgnisation controller

--- src/controllers/organisationController.ts
+++ src/controllers/organisationController.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 export class OrganisationController {
 
     async create_organisation(req: Request, res: Response) {
-        
+
         const userId = req.user?.userId as string
         logger.info(`User id from token - ${userId}`);
         const {name, description} = req.body;
@@ -109,6 +109,60 @@ export class OrganisationController {
             return res.status(400).json({
                 "status":"Bad Request",
                 "message": "Client error",
+                "statusCode": 400
+            })
+        }
+    }
+    
+    async addUser(req: Request, res: Response) {
+        
+        try {
+            const { orgId } = req.params;
+            const {userId} = req.body;
+
+        //check if user exists
+        const user = await prisma.user.findUnique({
+            where: {userId}
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                "status":"Bad Request",
+                "message": "User does not exist",
+                "statusCode": 400
+            });
+        }
+
+        //check if organisation exists
+        const organisation = await prisma.organisation.findUnique({
+            where: { orgId }
+        });
+
+        if (!organisation) {
+            return res.status(400).json({
+                "status": "Bad Request",
+                "message": "Organisation not found",
+                "statusCode": 400
+            });
+        }
+
+        //create the relatioship in the UserOrganisation table
+        await prisma.userOrganisation.create({
+            data: {
+                userId,
+                orgId
+            }
+        });
+
+        return res.status(200).json({
+            "status": "Success",
+            "message": "User added to organisation successfully"
+        })
+        
+        } catch (error:any) {
+            return res.status(400).json({
+                "status": "error",
+                "message": error.message,
                 "statusCode": 400
             })
         }
